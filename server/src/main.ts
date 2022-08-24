e18388786e8294db09c0cdf82f98ad9fc9b483fc
Raul <you@example.com>
2022-08-24 12:53:09 +0200

    commit init

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   //DEFINIMOS UN PREFIJO PARA LA API
   app.enableCors();
   app.setGlobalPrefix('api/v1');
-  await app.listen(4321);
+  await app.listen(process.env.PORT || 4321);
 }
 bootstrap();
